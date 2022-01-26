342f4a926a9962bd37e7db311380b58912877806
briannanayakkara <dimalbrian@gmail.com>
2022-01-26 17:26:40 +0100

    Program Done

---
+++ EthicalHacking/ConectionString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthicalHacking
+{
+    public class ConectionString
+    {
+        public static string cnn()
+        {
+            string con = @"Data Source=SOURCE-2-SERVER\NAV;Initial Catalog=SOLOSYS_TT;User ID=punchinout;Password=source2412;";
+
+            return con;
+        }
+    }
+}
--- EthicalHacking/Form1.cs
+++ EthicalHacking/Form1.cs
@@ -7,14 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace EthicalHacking
 {
     public partial class Form1 : Form
     {
+        public string q;
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        void ShowList(String Tbl)
+        {
+
+            dataGridView1.DataSource = GetDT(Tbl);
+        }
+        public  DataTable GetDT(string sp) // attach to savedata
+        {
+
+                try
+                {
+                    using (var cnn = new SqlConnection(ConectionString.cnn()))
+                    {
+
+                        cnn.Open();
+                        sp = @"select * from [source-1-server\dynamics].solotest.[dbo]." + sp;
+
+                        q = sp;
+
+                        SqlCommand cmd = new SqlCommand(q, cnn);
+                        cmd.CommandTimeout = 60; // 1 min 
+                        SqlDataReader sdr = cmd.ExecuteReader();
+                        DataTable dt = new DataTable();
+                        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                        dt.Load(sdr);
+                        //wait.Close();
+
+                        return dt;
+                    }
+                }
+                catch (SqlException)
+                {
+                    return null;
+                }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox1.Text == "Monitoring")
+            {
+                ShowList(textBox1.Text);
+                label1.Text = q;
+
+            }
+            else
+                MessageBox.Show("Access denied");
         }
     }
 }
